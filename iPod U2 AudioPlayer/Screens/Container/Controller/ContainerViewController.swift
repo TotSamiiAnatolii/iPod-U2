2007435f572cb2485301defec49131b977bebe50
Anatolii <tasamayapochta@rambler.ru>
2023-03-14 11:20:36 +0300

    project initialization

---
+++ iPod U2 AudioPlayer/Screens/Container/Controller/ContainerViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  iPod U2 AudioPlayer
+//
+//  Created by APPLE on 14.03.2023.
+//
+
+import UIKit
+
+final class ContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
